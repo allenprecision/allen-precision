07795e9a09a372a91367963996db14aa905a079b
KarimGilDev <karim.gilani@e-bizsoft.com>
2025-03-04 08:51:08 -0500

    added new view to remove fa-fw class

--- website_grouped_product/__manifest__.py
+++ website_grouped_product/__manifest__.py
@@ -24,8 +24,8 @@ Combo Products,
     'depends': ['website_sale_stock','website_sale_wishlist','website_sale_comparison', 'web', 'website_sale', ],
     'data': [
         'security/ir.model.access.csv',
-        # 'views/product_views.xml',
-        # 'views/templates.xml'
+        'views/product_views.xml',
+        'views/templates.xml'
 
     ],
     'assets': {
