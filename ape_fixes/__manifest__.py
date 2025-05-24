30a4c61bade37f7964635ed32c7fab1ce92941a9
KarimGilDev <karim.gilani@e-bizsoft.com>
2025-05-24 10:50:59 -0400

    [ADD] stock_delivery module in depends for APE

--- ape_fixes/__manifest__.py
+++ ape_fixes/__manifest__.py
@@ -13,7 +13,7 @@ And to add some new generic fields
 """,
     'author': 'Confianz Global,Inc.',
     'website': 'https://www.confianzit.com',
-    'depends': ['sale_management','sale_renting','stock','sale','delivery','account_reports','l10n_us_check_printing'],
+    'depends': ['sale_management','sale_renting','stock','sale','delivery','account_reports','l10n_us_check_printing', 'stock_delivery'],
     'data': [
         'report/purchase_order.xml',
         'report/sale_report.xml',
